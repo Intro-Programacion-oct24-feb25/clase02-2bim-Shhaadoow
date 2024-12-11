18e341edf9c323e44a559aea9bf10f6fa8b5d2f4
Pablo Ordóñez <pablosebas61@gmail.com>
2024-12-11 12:09:10 -0500

    Cambios

--- Ejemplos/src/paquete2/Ejemplo03.java
+++ Ejemplos/src/paquete2/Ejemplo03.java
@@ -5,7 +5,7 @@
  */
 package paquete2;
 
-import paquete1.*;
+
 
 /**
  *
@@ -18,22 +18,10 @@ public class Ejemplo03 {
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-
-        // arreglo de tipo entero
-        int[] arreglo1 = new int[5];
-        
-        // arreglo de tipo entero
-        int[] arreglo2;
-        arreglo2 = new int[5];
-        
-        // arreglo de tipo entero
-        int[] arreglo3 = {5, 10, 15, 20, 25}; // sin new
-        
-        // arreglo de doubles - 3 elementos
-        double[] arreglo4 = {5.1, 10.2, 15.2}; // sin new
         
         // arreglo de cadenas - 4 elementos
         String[] arreglo5 = {"Loja", "Quito", "Zamora", "Machala"};
+        System.out.println(arreglo5);
     }
 
 }
